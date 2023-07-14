f88616a7b0de838d4825f94932e044e843b4ca15
tlparche <h010508@naver.com>
2023-07-15 04:32:48 +0900

    add condition

--- pages/api/getNotice.js
+++ pages/api/getNotice.js
@@ -1,17 +1,30 @@
 import connection from '@/lib/mysql';
 
 export default async function handler(req, res) {
-    try {
-        const [rows, fields] = await connection.execute('SELECT id, title, description FROM notice');
-        const data = rows.map(row => ({
-            id: row.id,
-            title: row.title,
-            description: row.description
-        }));
+    const {method, query} = req;
 
-        res.status(200).json(data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, message: 'Failed to fetch data from DB' });
+    if (method === "GET") {
+        try {
+            let rows = [];
+            if (query?.id) {
+                const id = parseInt(query.id); // 정수형으로 변환
+                const [result, fields] = await connection.execute('SELECT id, title, description FROM notice WHERE id = ?', [id]);
+                rows = result;
+            } else {
+                const [result, fields] = await connection.execute('SELECT id, title, description FROM notice');
+                rows = result;
+            }
+            const data = rows.map(row => ({
+                id: row.id,
+                title: row.title,
+                description: row.description
+            }));
+            res.status(200).json(data);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({error: 'Internal Server Error'});
+        }
+    } else {
+        res.status(405).json({error: 'Method Not Allowed'});
     }
 }
